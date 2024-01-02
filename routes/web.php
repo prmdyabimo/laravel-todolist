b5e6a0db8846c11cf38b74631a846fc9113a252d
prmdyabimo <pramudyabimo1920@gmail.com>
2024-01-02 17:53:19 +0700

    add template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// TESTING TEMPLATE
+Route::get('/template', function () {
+    return view('template');
+});
